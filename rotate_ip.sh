9feb403a6a02046bd0818f975f92e59f93bfa94c
kschen202115 <124901446+kschen202115@users.noreply.github.com>
2026-03-13 15:19:16 +0800

    Update rotate_ip.sh

--- rotate_ip.sh
+++ rotate_ip.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-warp-proxy \
---bind 0.0.0.0:1080 \
---country US &
-
-sleep 3
-
-bash /rotate_ip.sh &
-
 while true
 do
-  bash /healthcheck.sh
-  sleep 60
+  sleep ${ROTATE_INTERVAL}
+
+  pkill warp-proxy
+
+  warp-proxy \
+  --bind 0.0.0.0:1080 \
+  --country US &
+
+  echo "IP rotated"
 done
